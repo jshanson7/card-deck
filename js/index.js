0319090b30b6c1866fb81d53a9e41af106ac9892
Jeff Hanson <jshanson7@gmail.com>
2015-07-29 14:18:10 -0700

    add select hand button, refactor

--- js/index.js
+++ js/index.js
@@ -1,172 +1,215 @@
 var VALUES = 'A,2,3,4,5,6,7,8,9,10,J,Q,K'.split(',');
-var SUITS = ['♣','♠','♥','♦'];
+var SUITS = ['♣', '♠', '♥', '♦'];
 
-function Base(options) {
-  _.extend(this, _.result(this, 'defaults'), options);
+function BaseClass(options) {
+  _.assign(this, _.result(this, 'defaults'), options);
   _.bindAll(this);
 }
-function Card() { Base.apply(this, arguments); }
-function Deck() { Base.apply(this, arguments); }
-function CardView() { Base.apply(this, arguments); }
-function DeckView() { Base.apply(this, arguments); }
 
-_.extend(Card.prototype, {
+function Card() {
+  BaseClass.apply(this, arguments);
+}
+
+Card.prototype = {
   defaults: {
-    'value': null,
-    'suit': null,
-    'selected': false
+    value: null,
+    suit: null,
+    selected: false
   },
-  getStrength: function () {
+  getStrength: function() {
     return VALUES.indexOf(this.value);
   },
-  setSelected: function (value) {
+  setSelected: function(value) {
     this.selected = value;
+    return this;
   },
-  toggleSelected: function () {
-    this.setSelected(!this.selected);
+  toggleSelected: function() {
+    return this.setSelected(!this.selected);
   }
-});
+};
 
-_.extend(Deck.prototype, {
-  defaults: function () {
+function Deck() {
+  BaseClass.apply(this, arguments);
+}
+
+Deck.prototype = {
+  defaults: function() {
     return {
-      'cards': _.flatten(_.map(VALUES, function (val) {
-        return _.map(SUITS, function (suit) {
-          return new Card({'value': val, 'suit': suit});
-        });
-      }))
+      cards: _.reduce(VALUES, function(cards, val) {
+        return cards.concat(SUITS.map(function(suit) {
+          return new Card({
+            value: val,
+            suit: suit
+          });
+        }));
+      }, [])
     };
   },
-  getCardsForSuit: function (suit) {
-    return _.where(this.cards, { 'suit': suit });
+  getCardsForSuit: function(suit) {
+    return _.where(this.cards, {
+      suit: suit
+    });
   },
-  selectHand: function (numberOfCards) {
+  selectHand: function(numberOfCards) {
     _(this.cards)
+      .invoke('setSelected', false)
       .sample(numberOfCards)
       .invoke('setSelected', true)
       .run();
   }
-});
+};
 
-_.extend(CardView.prototype, {
-  defaults: { 'card': null },
-  events: function () {
-    return {
-      'click': _.flow(this.card.toggleSelected, this.onSelectionToggle)
-    };
+function CardView() {
+  BaseClass.apply(this, arguments);
+}
+
+CardView.prototype = {
+  defaults: {
+    card: null
   },
-  onSelectionToggle: function () {
-    this.$el.toggleClass('selected', this.card.selected);
-  },
-  renderBigIcon: function () {
+  renderBigIcon: function() {
     return $('<div/>', {
       class: 'big-icon',
       html: this.card.suit
     });
   },
-  renderIcon: function () {
+  renderIcon: function() {
     return $('<div/>', {
       class: 'card-icon',
       html: this.card.suit
     });
   },
-  renderIcons: function () {
+  renderIcons: function() {
     var cardStrength = this.card.getStrength();
     return $('<div/>', {
       class: 'card-icons',
       html: cardStrength !== 0 && cardStrength < 10 ?
-        _.map(_.range(cardStrength + 1), this.renderIcon) :
-        this.renderBigIcon()
+        _.range(cardStrength + 1).map(this.renderIcon) : this.renderBigIcon()
     });
   },
-  renderValue: function (position) {
+  renderValue: function(position) {
     return $('<div/>', {
       class: 'card-value card-value-' + position,
       html: this.card.value
     });
   },
-  render: function () {
+  render: function() {
     var $old = this.$el;
-    
+
     this.$el = $('<div/>', {
-      class: 'card suit-' + this.card.suit + ' value-' + this.card.value +
+      class: 'card suit-' + this.card.suit +
+        ' value-' + this.card.value +
         (this.card.selected ? ' selected' : ''),
       html: [
         this.renderValue('top'),
         this.renderIcons(),
         this.renderValue('bottom')
       ],
-      on: _.result(this, 'events')
+      click: _.flow(this.card.toggleSelected, this.render)
     });
-    
+
     if ($old) {
-      this.$el.insertAfter( $old.off() );
-      $old.remove();
+      this.$el.insertAfter($old);
+      $old.off().remove();
     }
     return this.$el;
   }
-});
+};
 
-_.extend(DeckView.prototype, {
-  defaults: function () { return { 'deck': new Deck() }; },
-  events: function () {
+function DeckView() {
+  BaseClass.apply(this, arguments);
+}
+
+DeckView.prototype = {
+  defaults: function() {
     return {
-      'click .card': this.renderSelectionCount
+      deck: new Deck()
     };
   },
-  renderSelectionCount: function () {
+  selectHand: function(numberOfCards) {
+    this.deck.selectHand(_.isNumber(numberOfCards) ? numberOfCards : 5);
+    this.render();
+  },
+  renderMenu: function() {
+    return $('<div/>', {
+      class: 'menu',
+      html: [
+        this.renderSelectionCount(),
+        this.renderSelectHandButton()
+      ]
+    })
+  },
+  renderSelectHandButton: function() {
+    var $old = this.$selectHand;
+
+    this.$selectHand = $('<button/>', {
+      class: 'select-hand',
+      html: 'Select Hand',
+      click: this.selectHand
+    });
+
+    if ($old) {
+      this.$selectHand.insertAfter($old);
+      $old.remove();
+    }
+    return this.$selectHand;
+  },
+  renderSelectionCount: function() {
     var $old = this.$selectionCount;
 
-    this.$selectionCount = $('<div/>', {
+    this.$selectionCount = $('<span/>', {
       class: 'selection-count',
       html: 'Selected: ' +
-        _.where( this.deck.cards, {'selected': true}).length
+        _.where(this.deck.cards, {
+          selected: true
+        }).length
     });
-    
+
     if ($old) {
-      this.$selectionCount.insertAfter( $old );
+      this.$selectionCount.insertAfter($old);
       $old.remove();
     }
     return this.$selectionCount;
   },
-  renderCard: function (card) {
-    return new CardView({'card': card}).render();
+  renderCard: function(card) {
+    return new CardView({
+      card: card
+    }).render();
   },
-  renderCardRow: function (cards) {
+  renderCardRow: function(cards) {
     return $('<div/>', {
       class: 'card-row',
-      html: _.map(cards, this.renderCard)
+      html: cards.map(this.renderCard)
     });
   },
-  renderDeckBySuits: function () {
+  renderDeckBySuits: function() {
     var renderCardRowForSuit = _.compose(
       this.renderCardRow,
       this.deck.getCardsForSuit
     );
-    return _.map(SUITS, renderCardRowForSuit);
+    return SUITS.map(renderCardRowForSuit);
   },
-  render: function () {
+  render: function() {
     var $old = this.$el;
-    
+
     this.$el = $('<div/>', {
       class: 'deck-view',
       html: [].concat(
-        this.renderDeckBySuits(),
-        this.renderSelectionCount()
+        this.renderMenu(),
+        this.renderDeckBySuits()
       ),
-      on: _.result(this, 'events')
+      click: this.renderSelectionCount
     });
-    
+
     if ($old) {
-      this.$el.insertAfter($old.off());
-      $old.remove();
+      this.$el.insertAfter($old);
+      $old.off().remove();
     }
     return this.$el;
   }
-});
+};
 
-var deck = new Deck();
-var deckView = new DeckView({'deck': deck});
-deck.selectHand(5);
+var deckView = new DeckView();
+deckView.selectHand(5);
 
 $('#content').html(deckView.render());
